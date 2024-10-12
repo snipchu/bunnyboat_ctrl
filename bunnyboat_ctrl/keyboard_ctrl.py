5a70cd424b47eacdb9dfbb93c4cfd0b245343fd3
bunnyguy <pipandw@gmail.com>
2024-10-12 01:54:05 +0000

    Gazebo keyboard ctrl works nowgit add .git add .git add .git add .git add .WOOOOOOOOOOOOOOgit add .git add .git add .git add .git add .git add .!

--- bunnyboat_ctrl/keyboard_ctrl.py
+++ bunnyboat_ctrl/keyboard_ctrl.py
@@ -2,30 +2,55 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import Int32
+from std_msgs.msg import Float64
 
 
 class keyboardlistener(Node):
 
     def __init__(self):
         super().__init__('keyboard_ctrl')
+
+        # subscribe to /keyboard/keypress topic
         self.subscription = self.create_subscription(
             Int32,
             '/keyboard/keypress',
             self.listener_callback,
             10)
-        self.subscription  # prevent unused variable warning
+
+        # publish to gazebo thrusters topic
+        self.leftpub = self.create_publisher(
+                Float64,
+                '/wamv/thrusters/left/thrust',
+                10)
+        self.rightpub = self.create_publisher(
+                Float64,
+                '/wamv/thrusters/right/thrust',
+                10)
 
     def listener_callback(self, msg):
-        #self.get_logger().info('I heard: "%s"' % msg.data)
+        leftspeed=Float64()
+        rightspeed=Float64()
+        leftspeed.data=0.0
+        rightspeed.data=0.0
+        # keyboard input
         match msg.data:
             case 87: #w
                 self.get_logger().info('move forward')
+                leftspeed.data=1500.0;
+                rightspeed.data=1500.0;
             case 65: #a
                 self.get_logger().info('move left')
+                rightspeed.data=1500.0;
             case 83: #s
                 self.get_logger().info('move backward')
+                leftspeed.data=-1500.0;
+                rightspeed.data=-1500.0;
             case 68: #d
-                self.get_logger().info('move backward')
+                self.get_logger().info('move right')
+                leftspeed.data=1500.0;
+        # publish speeds to thruster topics
+        self.leftpub.publish(leftspeed)
+        self.rightpub.publish(rightspeed)
 
 
 def main(args=None):
